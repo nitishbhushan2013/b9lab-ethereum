656d3cfe7f9b6cf0fccb4665b7f3c2d535d48f53
Nitish Bhushan <nitishbhushan2013@gmail.com>
2018-11-26 18:40:33 +1100

    Remittence - deadline and kill

--- DAPPS/truffle/remittence/contracts/Remittence.sol
+++ DAPPS/truffle/remittence/contracts/Remittence.sol
@@ -6,20 +6,36 @@ contract Remittence {
     address public owner;
     uint public amount;
     bytes32 puzzleSecretvalue;
+    uint creationTime;
+    uint deadLineLimit;
 
     event LogContractInitialized(address byWhom, uint amountDeposited);
     event LogWithdrawRemittenceAmountInvoked(address from);
     event LogWithdrawRemittenceAmountSuccessed(address from, bytes32 password1, bytes32 password2, uint amountWithdrawan);
+    event LogWithDrawUnclaimedAmountCompleted(address byWhom, uint howMuch);
+    event LogContractDistruct(address owner);
+    
+    modifier onlyBy(address _owner) {
+        require(_owner == owner);
+        _;
+    }
+
+    modifier onlyAfter(uint _deadLineLimit){
+        require(now > _deadLineLimit);
+        _;
+    }
+
 
     // Alice has deployed the contract with ether in it and puzzle
-    constructor () public payable{
+    constructor (uint  _deadLineLimit) public payable{
         require(msg.value > 0, "ether amount must be greater than 0");
 
         owner = msg.sender;
         amount = msg.value;
-
+        creationTime = now;
+        deadLineLimit = _deadLineLimit;
         /**Let the secrert key be part of the contract code. This way, keys won't exposed to the outside
-        worls through anyways. Since Alice is deploying this contract, she knows these keys */
+        world through anymeans. Since Alice is deploying this contract, she knows these keys */
         puzzleSecretvalue = keccak256(abi.encodePacked("alice123", "123alice")); 
         emit LogContractInitialized(owner, amount);
     }
@@ -27,6 +43,7 @@ contract Remittence {
     /**This is a public function and hence can be invoked by any person.  */
     function withdrawRemittenceAmount(bytes32 password_bob, bytes32 password_carol) public payable returns(bool) {
         require(msg.sender != address(0), "contract invoking address must not be zero address");
+        require(amount > 0, "withdrawal amount must be more than 0.");
 
         emit LogWithdrawRemittenceAmountInvoked(msg.sender);
         bytes32 hashValue = keccak256(abi.encodePacked(password_bob,password_carol));
@@ -35,6 +52,19 @@ contract Remittence {
             msg.sender.transfer(amount);
         }
         emit LogWithdrawRemittenceAmountSuccessed(msg.sender,password_bob,password_carol,amount);
+        return true;
+    }
+
+    function withdrawUnclaimedEther() private onlyBy(owner) onlyAfter(creationTime+deadLineLimit) returns(bool) {
+        require(amount > 0, "ether must be there to withdraw");
+        emit LogWithDrawUnclaimedAmountCompleted(msg.sender, msg.value);
+
+        msg.sender.transfer(msg.value);
+        return true;
+    }
+
+    function kill() public onlyBy(owner) returns(bool){
+        selfdestruct(owner);
         return true;
     }
 
