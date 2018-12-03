478a5aeb4b5a5ae276d00e1556f8711127adebe3
Nitish Bhushan <nitishbhushan2013@gmail.com>
2018-12-03 13:49:42 +1100

    implemented xavier comments and feedback

--- DAPPS/truffle/firstProject/contracts/Splitter2.sol
+++ DAPPS/truffle/firstProject/contracts/Splitter2.sol
@@ -10,18 +10,22 @@ contract Splitter2{
     mapping(address => uint) balance;
  
     event LogAddressInitialized(address Alice, address Bob, address Carol);
-    event LogDepositMode(bytes32 mode); // split mode or single mode
+
+     // commented below as it does not reflect any state change.
+    //event LogDepositMode(bytes32 mode); // split mode or single mode
     event LogFundsDeposited(address indexed from, uint amount);
     event LogFundsWithdrawn(address indexed from, uint amount);
     event LogFundSplit(address indexed from, uint amount, address indexed receiver1, address indexed receiver2);
+    event LogLeftOverFundSent(address indexed to, uint amount);
     event LogContractDistruct(address owner);
+   
     
     constructor(address[3] memory addrs) public{
         require(addrs[0] != address(0), "addres must not be zero account");
         require(addrs[1] != address(0), "addres must not be zero account");
         require(addrs[2] != address(0), "addres must not be zero account");
      
-        owner = msg.sender; // this is contract owner who will receive amount if other than Alice send the deposit amount 
+        owner = msg.sender; 
    
         Alice = addrs[0];
         Bob = addrs[1];
@@ -29,23 +33,29 @@ contract Splitter2{
      
         emit LogAddressInitialized(Alice, Bob, Carol);
     }
-  
+  /**
+  whenever Alice sends ether to the contract for it to be split, half of it goes to Bob and the other half to Carol.
+   */
     function deposit () public payable returns(bool){
         require(msg.value > 0, "deposit amount must be positive number"); 
+        require(msg.sender == Alice, "Only Aice can invoke this function"); // this is tight binding with the given specification. 
         
-        if(msg.sender == Alice) { // Bob and Carol would recieve equal amount 
-            emit LogDepositMode("split mode deposit");
-            splitFund(Bob, Carol);
-        }
-        else { // contract would receive the amount 
-            emit LogDepositMode("single deposit");
-            balance[msg.sender] += msg.value; 
-            emit LogFundsDeposited(msg.sender,msg.value);
+        uint splitAmount = msg.value/2;  
+        uint leftOverAmount = msg.value - (2*splitAmount);
+
+        emit LogFundSplit(msg.sender,splitAmount,Bob, Carol);
+   
+        balance[Bob] += splitAmount;
+        balance[Carol] += splitAmount;
+       
+        if(leftOverAmount > 0){ // its not wallet and its bad to leave unused amount in the contract. return it back to Alice.
+            emit  LogLeftOverFundSent(msg.sender, leftOverAmount);
+            msg.sender.transfer(leftOverAmount);
         }
         return true;
     }
  
-    function withdrawal() public payable returns(bool){
+    function withdrawal() public returns(bool){
         uint totalAmount = balance[msg.sender];
         require(totalAmount > 0, "must be enough amount");
         
@@ -57,26 +67,7 @@ contract Splitter2{
         return true;
     }
  
-    function splitFund (address receiver1, address receiver2) public payable returns(bool) {
-        require(msg.value > 0, "amount must be positive number");
-        require(receiver1 != address(0), "addres must not be zero account");
-        require(receiver2 != address(0), "addres must not be zero account");
-       
-        /* no need for this check, as division takes the floor anyway. 
-        if(amount % 2 != 0) { // if its not even, get the even. 1 wei would send back to contract 
-            amount = amount - 1;
-        }*/ 
-        uint splitAmount = msg.value/2;  
-        
-        emit LogFundSplit(msg.sender,splitAmount,receiver1, receiver2);
-        
-        balance[receiver1] += splitAmount;
-        balance[receiver2] += splitAmount;
-       
-        return true;
-    }
- 
-    function getBalance(address addr) public view  returns(uint){ // this will not change the world state
+    function getBalance(address addr) public view returns(uint){ // this will not change the world state
         return balance[addr];
     }
     
