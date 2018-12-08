45ac769cb299c54ae06b9e228847f069eb94e061
Nitish Bhushan <nitishbhushan2013@gmail.com>
2018-12-08 12:26:31 +1100

    RemittenceV1: implemented Xavier comments

--- DAPPS/truffle/remittence/contracts/RemittenceV1.sol
+++ DAPPS/truffle/remittence/contracts/RemittenceV1.sol
@@ -12,20 +12,20 @@ contract RemittenceV1 is Pausable{
     address public Carol;
     // This holds all details about remittence
     struct Remittence {
-        bytes remittenceName; // acts as a unique identifier
         bytes32 puzzle;
         uint amount;
-        uint duration;
+        uint deadLine;
         bool hasPuzzleSet;
+        //uint index;
     }
-
     // Allow us to look the remittence details by remittence name
     mapping(bytes => Remittence) public remittenceMap;
+   // bytes[] remittenceNameIndex;
 
-    //ensure unique remittence name
+    //ensure unique remittence name. 
     mapping(bytes => bool) public uniqueNameMap;
 
-    event LogContractConditionInitialized(address from, string indexed remittenceName, uint amount, uint duration);
+    event LogContractConditionInitialized(address from, string indexed remittenceName, uint amount, uint deadLine);
     event LogWithdrawRemittenceAmountSuccessed(address indexed byWhom, string indexed remittenceName, uint indexed withdrawalAmount);
     event LogWithdrawUnclaimedAmountCompleted(address byWhom, string indexed remittenceName, uint indexed withdrawalAmount);
     event LogContractDistruct(address owner);
@@ -50,33 +50,28 @@ contract RemittenceV1 is Pausable{
     constructor () public {
         require(msg.sender == getOwner(), "only owner who deployed Ownable contract can deploy this contract ");
     }
+
+    
     /**
     @dev This function would set the contract conditions and can be called only by the Alice, who is the contract owner.
-    @param _Count total number of remittence submitted by Alice
-    @param _nameArray array of remittence name. Each remittence is identified by its name
-    @param _puzzleArray array of hashed puzzle for each remittence
-    @param _amountArray array of amount for each remittence
-    @param _durationArray array of duration after which Alice can withdraw the ether
+    Alice can call this function any number of time to set the contract with the limitation of not reusing the same
+    contract name until it is active. 
+    @param _name array of remittence name. Each remittence is identified by its name
+    @param _puzzle hashed puzzle for each remittence
+    @param _amount amount for each remittence
+    @param _duration duration after which Alice can withdraw the ether
     @param _CarolAddress Carol address, who in turn would solve the puzzle
     */
-    //TODO: check conversion of bytes to string
-    function initializeContract(uint8 _Count, bytes[] _nameArray, bytes32[] _puzzleArray, uint[] _amountArray, uint[]  _durationArray, address _CarolAddress) public {
-        require(_Count == _nameArray.length, "count must be same");
-        require(_Count == _puzzleArray.length, "count must be same");
-        require(_Count == _amountArray.length, "count must be same");
-        require(_Count == _durationArray.length, "count must be same");
-
+    function setContractCondition(bytes _name, bytes32 _puzzle, uint _amount, uint _duration, address _CarolAddress) public {
+        require(!uniqueNameMap[_name], "This name is in active state"); 
         Carol = _CarolAddress;
-        // iterate each record and create the remittence
-        for(uint i = 0; i < _Count; i++){  
-            require(!uniqueNameMap[_nameArray[i]], "name must be unique"); // This ensures, there won't be any duplicate
-            Remittence memory  newRemittenceRecord = Remittence(_nameArray[i], _puzzleArray[i], _amountArray[i], now + _durationArray[i] * 1 seconds, true);    
-           // remittenceArray.push(newRemittenceRecord);
-            remittenceMap[_nameArray[i]] = newRemittenceRecord;
-            emit LogContractConditionInitialized(msg.sender, string(_nameArray[i]), _amountArray[i], _durationArray[i]);
-        }
+        uint timeLine = now + _duration * 1 seconds;
+        Remittence memory newRemittenceRecord = Remittence(_puzzle, _amount, timeLine, true);    
+        remittenceMap[_name] = newRemittenceRecord;
+        uniqueNameMap[_name] = true;
+        emit LogContractConditionInitialized(msg.sender, string(_name),_amount, timeLine);
     }
-    
+
     /**
     @dev This function must be invoked by Carol to solve the puzzle and withdraw the amount.
     @param remittenceName  name of remittence. Carol wants to withdraw amount for this remittence
@@ -87,17 +82,18 @@ contract RemittenceV1 is Pausable{
     function withdrawRemittenceAmount(string memory remittenceName, bytes32 password_bob, bytes32 password_carol)
     public
     onlyWhenNotPaused
-    onlyOnBeforeDeadLine(remittenceMap[bytes(remittenceName)].duration)
+    onlyOnBeforeDeadLine(remittenceMap[bytes(remittenceName)].deadLine)
     returns(bool) {
         require(msg.sender == Carol, "only Carol can invoke this function");
         require(remittenceMap[bytes(remittenceName)].hasPuzzleSet, "puzzle is not set. This remittence is invalid");
-        require(remittenceMap[bytes(remittenceName)].amount > 0, "withdrawal amount must be more than 0.");
         require(remittenceMap[bytes(remittenceName)].puzzle == keccak256(abi.encodePacked(password_bob,password_carol)),"password mismatch,can not withdraw the amount");
         uint withdrawalAmount;
         //accounting
         withdrawalAmount = remittenceMap[bytes(remittenceName)].amount;
+
         remittenceMap[bytes(remittenceName)].amount = 0; // counter reentrant attack
         remittenceMap[bytes(remittenceName)].hasPuzzleSet = false; // allow reusbility of the contract.
+        uniqueNameMap[_name] = false; // Now Alice can use this name again in future remittence.
         emit LogWithdrawRemittenceAmountSuccessed(msg.sender, remittenceName, withdrawalAmount);
         msg.sender.transfer(withdrawalAmount);
         return true;
@@ -110,19 +106,20 @@ contract RemittenceV1 is Pausable{
     public
     onlyWhenNotPaused
     onlyOwner
-    onlyAfterDeadLine(remittenceMap[bytes(remittenceName)].duration)
+    onlyAfterDeadLine(remittenceMap[bytes(remittenceName)].deadLine)
     returns(bool) {
         require(remittenceMap[bytes(remittenceName)].hasPuzzleSet, "puzzle is not set or remittence has been successfully used by carol");
-        require(remittenceMap[bytes(remittenceName)].amount > 0, "ether must be there to withdraw");
         uint withdrawalAmount;
         //accounting
         withdrawalAmount = remittenceMap[bytes(remittenceName)].amount;
         remittenceMap[bytes(remittenceName)].amount = 0;
         remittenceMap[bytes(remittenceName)].hasPuzzleSet = false;
+        uniqueNameMap[_name] = false; // Now Alice can use this name again in future remittence.
         emit LogWithdrawUnclaimedAmountCompleted(msg.sender, remittenceName, withdrawalAmount);
         msg.sender.transfer(withdrawalAmount);
         return true;
     }
+    
     
     function kill()
     public
